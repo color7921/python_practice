d59758df2464fbf95e964be9acfa8e260d937b20
color7921 <132988693+color7921@users.noreply.github.com>
2024-02-05 22:04:50 +0900

    coding test
    coding test

--- Programmers/sort.py
+++ Programmers/sort.py
@@ -1,10 +1,30 @@
-arr = [6, 2, 10]
+# 2. 가장 큰 수
 
-answers = ''
+# arr = [6, 2, 10]
 
-arr = list(map(str, arr))
-arr.sort(key = lambda x : x*3,reverse=True)
+# answers = ''
 
-for i in arr:
-    answers += i
-print(str(int(answers)))
+# arr = list(map(str, arr))
+# arr.sort(key = lambda x : x*3,reverse=True)
+
+# for i in arr:
+#     answers += i
+# print(str(int(answers)))
+
+# 깨달은 점
+# lambda 사용법, 반복문과 문자열의 관계
+
+###################################
+
+# 3. H-Index
+
+citations = [2, 0, 2, 1, 1]
+
+citations.sort(reverse=True)
+
+for i in range(len(citations)):
+    if citations[i] < i + 1:
+        print(i)
+        break
+# print(len(citations))
+    
