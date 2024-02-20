8a1e00ad7439dc99aad0d4dbf2faf1ea90a3b03b
color7921 <132988693+color7921@users.noreply.github.com>
2024-02-20 23:14:32 +0900

    target number
    BFS

--- Programmers/practice.py
+++ Programmers/practice.py
@@ -1,8 +1,13 @@
 ## JadenCase 문자열 만들기
 
-s = "3people unFollowed me"
+# s = "3people unFollowed me"
 
-a = s.split()
-b = ''
-for i in range(len(a)):
-    b += a[i][0].upper() + a[i][1::] + " "
+# a = s.split()
+# b = ''
+# for i in range(len(a)):
+#     b += a[i][0].upper() + a[i][1::].lower() + " "
+
+## 타겟 넘버
+numbers = [1, 1, 1, 1, 1]
+target = 3
+
