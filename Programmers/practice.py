734ef08aebe508b6ef8225c68ec6281cdc5850e1
color7921 <132988693+color7921@users.noreply.github.com>
2024-02-23 17:06:12 +0900

    correct string
    - correct string

--- Programmers/practice.py
+++ Programmers/practice.py
@@ -8,17 +8,46 @@
 #     b += a[i][0].upper() + a[i][1::].lower() + " "
 
 ## 타겟 넘버
-numbers = [1, 1, 1, 1, 1]
-target = 3
+# numbers = [1, 1, 1, 1, 1]
+# target = 3
 
 ## 최솟값 만들기
 
-A = [1, 4, 2]
-B = [5, 4, 4]
+# A = [1, 4, 2]
+# B = [5, 4, 4]
 
-A.sort(reverse=True)
-B.sort()
-answer = 0
-for i in range(len(A)):
-    answer += (A[i]*B[i])
-print(answer)
+# A.sort(reverse=True)
+# B.sort()
+# answer = 0
+# for i in range(len(A)):
+#     answer += (A[i]*B[i])
+# print(answer)
+
+## 올바른 괄호
+# s = "()("
+# arr = list(s)
+# count = 0
+# for i in range(len(arr)):
+#     if arr[i] == '(':
+#         count += 1
+#     else :
+#         count -= 1
+# if count == 0:
+#     print(True)
+# else :
+#     print(False)
+
+s = "()("
+count = 0
+for i in s:
+    if i == '(':
+        count += 1
+    else:
+        if count > 0:
+            count -= 1
+        else:
+            print(False)
+if count == 0:
+    print(True)
+else:
+    print(False)
